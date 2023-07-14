0696662a3b25dfa8b8d9d45faa79ee1a936bd875
spikethea <spike00890@hotmail.co.uk>
2023-07-14 18:39:06 +0100

    responsiveness & get involved page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Glide from '@glidejs/glide';
 
+const heroSection = document.querySelector('#hero-section');
+const backToTop = document.querySelector('#back-to-top');
+
 let MMECardCarousel = null;
 
 window.addEventListener('load', function () {
@@ -9,9 +12,32 @@ window.addEventListener('load', function () {
     gap: 40,
     perView: 3,
     breakpoints: {
-        800: {
-            perView: 1
+        480: {
+            perView: 1,
+            gap:10,
+            peek: 20
+        },
+        820: {
+            perView: 2,
+            gap: 40,
+            peek: 0
         }
+
     }
     }).mount();
-  });
+});
+
+window.addEventListener('scroll', () => {
+    if (
+        window.scrollY > heroSection.clientHeight &&
+        window.innerWidth < 481
+    ) {
+        backToTop.classList.remove('hidden');
+    } else {
+        backToTop.classList.add('hidden');
+    }
+});
+
+window.addEventListener('resize', () => {
+    MMECardCarousel.remount()
+});
